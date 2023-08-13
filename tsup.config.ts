710a00d33944bfa2b97b631ce0623995ef60efaa
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-08-13 19:49:56 -0400

    Add initial tsconfig and tsup config

---
+++ tsup.config.ts
@@ -0,0 +1,18 @@
+import type { Options } from "tsup";
+
+const env = process.env.NODE_ENV;
+
+export const tsup: Options = {
+  splitting: true,
+  clean: true,
+  dts: true,
+  format: ["cjs", "esm"],
+  minify: env === "production",
+  bundle: env === "production",
+  skipNodeModulesBundle: true,
+  entryPoints: ["src/index.ts"],
+  watch: env === "development",
+  target: "es2020",
+  outDir: env === "production" ? "dist" : "lib",
+  entry: ["src/**/*.ts", "src/**/*.tsx"],
+};
