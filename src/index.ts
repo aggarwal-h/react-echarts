6fd2df42f5d37f275a5c6749eecfacdb003a88a3
Himanshu Aggarwal <aggarwal.h@outlook.com>
2023-08-13 21:15:09 -0400

    Export hook as default and as named-export

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,5 @@
 import useReactECharts from "./hooks";
 import ReactECharts from "./components";
 
-export { useReactECharts as default, ReactECharts };
+export { useReactECharts as default, useReactECharts, ReactECharts };
 export * from "./types";
